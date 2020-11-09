c46ca1ce4b16684c0fbb78c43d26fdccbceaa40b
DkeRee <dkeree1@gmail.com>
2020-11-09 19:28:05 +0000

    Added many things, check Update Log in the website

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-var crew = ["Mostepic", "Grembles", "LGT Godfather", "Captin Autisimo", "CPL Cardilog", "Table Salt", "AE", "Karma", "Potato Salad", "wetassmaster", "1479tds", "Ark Sentinel", "Deletus",]
+var crew = ["Mostepic", "Grembles", "LGT Godfather", "Captin Autisimo", "CPL Cardilog", "Table Salt", "AE", "Karma", "Potato Salad", "wetassmaster", "1479tds", "Ark Sentinel", "Deletus", "TickDaSpider (bitch the)", "Accorum", "TAnnert"]
 
 function pingCrew(){
   
-  document.getElementById("pulser-crew").innerHTML = crew[Math.floor(Math.random() * 13 + 0)]
+  document.getElementById("pulser-crew").innerHTML = crew[Math.floor(Math.random() * 16 + 0)]
 
+}
+
+var click = 0;
+
+function countClick(){
+ 
+ click++
+
+  document.getElementById("counter").innerHTML = "You have pinged " + click + " crew!";
+   
 }
 
