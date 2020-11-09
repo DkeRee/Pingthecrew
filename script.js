17a922f38b97b0c519344599c1070c2abe9c56fb
DkeRee <dkeree1@gmail.com>
2020-11-09 19:33:21 +0000

    Added one more person oops lmao

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var crew = ["Mostepic", "Grembles", "LGT Godfather", "Captin Autisimo", "CPL Cardilog", "Table Salt", "AE", "Karma", "Potato Salad", "wetassmaster", "1479tds", "Ark Sentinel", "Deletus", "TickDaSpider (bitch the)", "Accorum", "TAnnert", "hunter (best admin!!)"]
+var crew = ["Mostepic", "Grembles", "LGT Godfather", "Captin Autisimo", "CPL Cardilog", "Table Salt", "AE", "Karma", "Potato Salad", "wetassmaster", "1479tds", "Ark Sentinel", "Deletus", "TickDaSpider (bitch the)", "Accorum", "TAnnert", "hunter (best admin!!)", "Bigman"]
 
 function pingCrew(){
   
-  document.getElementById("pulser-crew").innerHTML = crew[Math.floor(Math.random() * 17 + 0)]
+  document.getElementById("pulser-crew").innerHTML = crew[Math.floor(Math.random() * 18 + 0)]
 
 }
 
