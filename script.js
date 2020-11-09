980b4dedf038383b96e32d3d4fc2662ebdf9a340
DkeRee <dkeree1@gmail.com>
2020-11-09 19:31:49 +0000

    Added a minor change to the number of "crew", my error

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var crew = ["Mostepic", "Grembles", "LGT Godfather", "Captin Autisimo", "CPL Cardilog", "Table Salt", "AE", "Karma", "Potato Salad", "wetassmaster", "1479tds", "Ark Sentinel", "Deletus", "TickDaSpider (bitch the)", "Accorum", "TAnnert"]
+var crew = ["Mostepic", "Grembles", "LGT Godfather", "Captin Autisimo", "CPL Cardilog", "Table Salt", "AE", "Karma", "Potato Salad", "wetassmaster", "1479tds", "Ark Sentinel", "Deletus", "TickDaSpider (bitch the)", "Accorum", "TAnnert", "hunter (best admin!!)"]
 
 function pingCrew(){
   
-  document.getElementById("pulser-crew").innerHTML = crew[Math.floor(Math.random() * 16 + 0)]
+  document.getElementById("pulser-crew").innerHTML = crew[Math.floor(Math.random() * 17 + 0)]
 
 }
 
