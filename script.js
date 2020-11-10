41947031297fcf5f6dc129213eb1b7c86858553c
DkeRee <dkeree1@gmail.com>
2020-11-10 02:29:38 +0000

    I added secret stuff that will pop up at button click milestones xD

--- script.js
+++ script.js
@@ -8,7 +8,31 @@ function pingCrew(){
 
 var click = 0;
 
+
 function countClick(){
+
+  
+if (click === 100){
+  alert("Arent you spending too much time on this?")
+}
+
+if (click === 500){
+  alert("Bruh, don't you have to go do some chores or something xD")
+  document.getElementById("flashbang").style.display = 'block';
+}
+
+if (click === 1000){
+  alert("get a life LOL")
+}
+
+if (click === 5000){
+  alert(";-;")
+}
+
+if (click === 10000){
+  alert("you HAVE to be using an autoclicker man xD. You deserve it, here take a waifu")
+  document.getElementById("waifu").style.display = 'block';
+}
  
  click++
 
